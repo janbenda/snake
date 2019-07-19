b4a045145de53ce6b134bccd81171cc0ad61bf50
Scheuer Jan <scheuer@email.cz>
2019-07-19 12:10:07 +0200

    jeste cisteni

--- isdoc/ZpracujISDOC.sh
+++ isdoc/ZpracujISDOC.sh
@@ -3,10 +3,6 @@
 #vystupni dbf (koncovka dbf se pridava)
 ISDOCFILE=$1
 OUTPFILE=$2
-#sed   's/\r$//' ${ISDOCFILE} >${ISDOCFILE}.crlf
-#sed -e :a -e '$!N; s/\n/ /; ta'  ${ISDOCFILE}.crlf >${ISDOCFILE}.crlf.oneline
-#vd --encoding cp1250 -o ${ISDOCFILE}.csv -f xml -b ${ISDOCFILE}.crlf.oneline
-#iconv -f UTF8 -t iso8859-2 -o ${ISDOCFILE}.csv.isowin  ${ISDOCFILE}.csv
 
 sed   's/\r$//' ${ISDOCFILE} | \
 sed -e :a -e '$!N; s/\n/ /; ta' >${ISDOCFILE}.crlf.oneline
